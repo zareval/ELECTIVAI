fc8d5e2405ec5f6530604752baf40279ee447bc3
Zare <zarellaburbanov@gmail.com>
2024-05-30 16:09:07 -0500

    series-listar listo

--- src/app/series/serie-listar/serie-listar.component.spec.ts
+++ src/app/series/serie-listar/serie-listar.component.spec.ts
@@ -1,18 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SerieListarComponent } from './serie-listar.component';
+import { SeriesCrearComponent } from './serie-listar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../../app.component';
 
+describe('AppComponent',() =>{
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations:[AppComponent]
+  }));
+
+  it('should create the app', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app).toBeTruthy();
+  })
+})
 describe('SerieListarComponent', () => {
-  let component: SerieListarComponent;
-  let fixture: ComponentFixture<SerieListarComponent>;
+  let component: SeriesCrearComponent;
+  let fixture: ComponentFixture<SeriesCrearComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SerieListarComponent]
+      imports: [SeriesCrearComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(SerieListarComponent);
+    fixture = TestBed.createComponent(SeriesCrearComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/series/serie-listar/serie-listar.component.ts
+++ src/app/series/serie-listar/serie-listar.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray,FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Series } from '../series';
+import { SeriesService } from '../series.service';
+
 
 @Component({
-  selector: 'app-serie-listar',
+  selector: 'app-crear-series',
   standalone: true,
-  imports: [],
-  templateUrl: './serie-listar.component.html',
-  styleUrl: './serie-listar.component.css'
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './crear-series.component.html',
+  styleUrl: './crear-series.component.css'
 })
-export class SerieListarComponent {
+export class SeriesCrearComponent implements OnInit {
+  seriesForm!: FormGroup;
+  constructor(private formBuilder : FormBuilder, private seriesService: SeriesService){}
+
+  ngOnInit(): void {
+      this.seriesForm = this.formBuilder.group({
+        nombre: ['', [Validators.required]],
+        nacionalidad: ['',[Validators.required]],
+        f_nacimiento: ['',[Validators.required]],
+        biografia: ['',[Validators.required]],
+      })
+  }
+
+  crearSeries(series: Series):void{
+    this.seriesService.crearSeries(series).subscribe(
+      (eriesCreada)=> {
+        alert('series creada con éxito');
+        this.seriesForm.reset();
+      },
+    );
+  }
 
 }
