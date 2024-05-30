4b0131bae33d0e239e77999c5d6bdb3a00f23d1f
Zare <zarellaburbanov@gmail.com>
2024-05-30 16:38:51 -0500

    series-crear listo

--- src/app/series/series-crear/series-crear.component.spec.ts
+++ src/app/series/series-crear/series-crear.component.spec.ts
@@ -1,18 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SeriesCrearComponent } from './series-crear.component';
+import { seriesCrearComponent } from './series-crear.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../../app.component';
 
-describe('SeriesCrearComponent', () => {
-  let component: SeriesCrearComponent;
-  let fixture: ComponentFixture<SeriesCrearComponent>;
+describe('AppComponent',() =>{
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations:[AppComponent]
+  }));
+
+  it('should create the app', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app).toBeTruthy();
+  })
+})
+describe('seriesCrearComponent', () => {
+  let component: seriesCrearComponent;
+  let fixture: ComponentFixture<seriesCrearComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SeriesCrearComponent]
+      imports: [seriesCrearComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(SeriesCrearComponent);
+    fixture = TestBed.createComponent(seriesCrearComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/series/series-crear/series-crear.component.ts
+++ src/app/series/series-crear/series-crear.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray,FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Series } from '../series';
+import { SeriesService } from '../series.service';
+
 
 @Component({
-  selector: 'app-series-crear',
+  selector: 'app-crear-series',
   standalone: true,
-  imports: [],
-  templateUrl: './series-crear.component.html',
-  styleUrl: './series-crear.component.css'
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './crear-series.component.html',
+  styleUrl: './crear-series.component.css'
 })
-export class SeriesCrearComponent {
+export class seriesCrearComponent implements OnInit {
+  seriesForm!: FormGroup;
+  constructor(private formBuilder : FormBuilder, private SeriesService: SeriesService){}
+
+  ngOnInit(): void {
+      this.seriesForm = this.formBuilder.group({
+        nombre: ['', [Validators.required]],
+        nacionalidad: ['',[Validators.required]],
+        f_nacimiento: ['',[Validators.required]],
+        biografia: ['',[Validators.required]],
+      })
+  }
+
+  crearSeries(series: Series):void{
+    this.SeriesService.crearSeries(series).subscribe(
+      (seriesCreada)=> {
+        alert('series creada con éxito');
+        this.seriesForm.reset();
+      },
+    );
+  }
 
 }
