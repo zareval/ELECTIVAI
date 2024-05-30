af4326549715fc9622ac734416ff8c9883464aa5
Zare <zarellaburbanov@gmail.com>
2024-05-30 11:34:17 -0500

    listar director listo

--- src/app/directores/listar-director/listar-director.component.ts
+++ src/app/directores/listar-director/listar-director.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Directores } from '../directores';
+import { DirectoresService } from '../directores.service';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {Router, RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-listar-director',
@@ -8,5 +13,25 @@ import { Component } from '@angular/core';
   styleUrl: './listar-director.component.css'
 })
 export class ListarDirectorComponent {
+  directores :Array<Directores>=[]
+a: any;
+  constructor(private routerPath: Router,private DirectoresService:DirectoresService){
+
+  }
+
+  ngOnInit(){
+    this.listarDirector();
+  }
+
+  listarDirector(){
+    this.DirectoresService.listarDirector().subscribe(vs=>{
+      this.directores=vs;
+      console.log(this.directores)
+    })
+  }
+
+  onEditarNavigate(id:number){
+    this.routerPath.navigate([`/vehiculos/editar/${id}`]);
+  }
 
 }
