e570c7c80061fd66e6f630fcc1352de2ef7f5f6f
Zare <zarellaburbanov@gmail.com>
2024-05-30 09:47:56 -0500

    crear-director listo

--- src/app/directores/crear-director/crear-director.component.spec.ts
+++ src/app/directores/crear-director/crear-director.component.spec.ts
@@ -1,18 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CrearDirectorComponent } from './crear-director.component';
+import { CreardirectorComponent } from './crear-director.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../../app.component';
+
+describe('AppComponent',() =>{
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations:[AppComponent]
+  }));
+
+  it('should create the app', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app).toBeTruthy();
+  })
+})
 
 describe('CrearDirectorComponent', () => {
-  let component: CrearDirectorComponent;
-  let fixture: ComponentFixture<CrearDirectorComponent>;
+  let component: CreardirectorComponent;
+  let fixture: ComponentFixture<CreardirectorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CrearDirectorComponent]
+      imports: [CreardirectorComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(CrearDirectorComponent);
+    fixture = TestBed.createComponent(CreardirectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/directores/crear-director/crear-director.component.ts
+++ src/app/directores/crear-director/crear-director.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray,FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Directores } from '../directores';
+import { DirectoresService } from '../directores.service';
+
 
 @Component({
   selector: 'app-crear-director',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './crear-director.component.html',
   styleUrl: './crear-director.component.css'
 })
-export class CrearDirectorComponent {
+export class CreardirectorComponent implements OnInit {
+  directorForm!: FormGroup;
+  constructor(private formBuilder : FormBuilder, private DirectoresService: DirectoresService){}
+
+  ngOnInit(): void {
+      this.directorForm = this.formBuilder.group({
+        nombre: ['', [Validators.required]],
+        nacionalidad: ['',[Validators.required]],
+        f_nacimiento: ['',[Validators.required]],
+        biografia: ['',[Validators.required]],
+      })
+  }
+
+  crearDirector(directores: Directores):void{
+    this.DirectoresService.crearDirector(directores).subscribe(
+      (directorCreado)=> {
+        alert('director creado con éxito');
+        this.directorForm.reset();
+      },
+    );
+  }
 
 }
