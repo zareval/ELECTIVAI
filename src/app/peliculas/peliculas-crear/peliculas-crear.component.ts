15d3981b409d4a415eb5ae6c04535c61512e4a1b
Zare <zarellaburbanov@gmail.com>
2024-05-30 15:20:49 -0500

    peliculas crear listo

--- src/app/peliculas/peliculas-crear/peliculas-crear.component.ts
+++ src/app/peliculas/peliculas-crear/peliculas-crear.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray,FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Peliculas } from '../peliculas';
+import { PeliculasService } from '../peliculas.service';
+
 
 @Component({
-  selector: 'app-peliculas-crear',
+  selector: 'app-crear-peliculas',
   standalone: true,
-  imports: [],
-  templateUrl: './peliculas-crear.component.html',
-  styleUrl: './peliculas-crear.component.css'
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './crear-peliculas.component.html',
+  styleUrl: './crear-peliculas.component.css'
 })
-export class PeliculasCrearComponent {
+export class PeliculasCrearComponent implements OnInit {
+  peliculasForm!: FormGroup;
+  constructor(private formBuilder : FormBuilder, private PeliculasService: PeliculasService){}
+
+  ngOnInit(): void {
+      this.peliculasForm = this.formBuilder.group({
+        nombre: ['', [Validators.required]],
+        nacionalidad: ['',[Validators.required]],
+        f_nacimiento: ['',[Validators.required]],
+        biografia: ['',[Validators.required]],
+      })
+  }
+
+  crearPeliculas(Peliculas: Peliculas):void{
+    this.PeliculasService.crearPeliculas(Peliculas).subscribe(
+      (peliculasCreada)=> {
+        alert('peliculas creada con éxito');
+        this.peliculasForm.reset();
+      },
+    );
+  }
 
 }
--- src/app/peliculas/peliculas.service.ts
+++ src/app/peliculas/peliculas.service.ts
@@ -1,9 +1,29 @@
+import { TestBed } from '@angular/core/testing';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Peliculas } from './peliculas';
+
+
+const API_URL=''
 
 @Injectable({
   providedIn: 'root'
 })
-export class PeliculasService {
+export class PeliculasService{
+constructor(private http:HttpClient){}
 
-  constructor() { }
+    listarPeliculas():Observable<Peliculas[]>{
+      return this.http.get<Peliculas[]>(API_URL);
+    }
+    crearPeliculas(Peliculas:Peliculas):Observable<Peliculas>{
+      return this.http.post<Peliculas>(API_URL, Peliculas)
+    }
+    editarPeliculas(Peliculas:Peliculas, idPeliculas:number):Observable<Peliculas>{
+      return this.http.put<Peliculas>('http://127.0.0.1:8000/api/peliculas/actualizacionPeliculas/1/'+idPeliculas, Peliculas)
+    }
+    eliminarPeliculas(idPeliculas: number): Observable<Peliculas> {
+      return this.http.delete<Peliculas>(''+idPeliculas);
+    }
+  
 }
