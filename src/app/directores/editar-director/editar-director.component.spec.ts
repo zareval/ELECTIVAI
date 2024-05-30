d90872ca5e998c3b1017dc2e1001da0b02bea69c
Zare <zarellaburbanov@gmail.com>
2024-05-30 09:53:59 -0500

    editar director LISTO

--- src/app/directores/editar-director/editar-director.component.spec.ts
+++ src/app/directores/editar-director/editar-director.component.spec.ts
@@ -2,6 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditarDirectorComponent } from './editar-director.component';
 
+import {RouterTestingModule} from '@angular/router/testing'
+import { AppComponent } from '../../app.component';
+
+describe('AppComponent',() =>{
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations:[AppComponent]
+  }));
+
+  it('should create the app', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app).toBeTruthy();
+  })
+})
 describe('EditarDirectorComponent', () => {
   let component: EditarDirectorComponent;
   let fixture: ComponentFixture<EditarDirectorComponent>;
--- src/app/directores/editar-director/editar-director.component.ts
+++ src/app/directores/editar-director/editar-director.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Directores } from '../directores';
+import { DirectoresService } from '../directores.service';
 
 @Component({
   selector: 'app-editar-director',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule], // Assuming you're using Reactive Forms
   templateUrl: './editar-director.component.html',
-  styleUrl: './editar-director.component.css'
+  styleUrls: ['./editar-director.component.css']
 })
-export class EditarDirectorComponent {
+export class EditarDirectorComponent implements OnInit {
+  directoresForm!: FormGroup;
+  directores!: Directores;
+  idDirector: string = '';
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private DirectoresService: DirectoresService
+  ) {
+    this.idDirector = this.route.snapshot.params['idDirector'];
+  }
+
+  ngOnInit(): void {
+    this.DirectoresService.listarDirector().subscribe(di => {
+      this.directores = di.find(Director =>Director.idDirector === parseInt(this.idDirector))!; // Get the specific director based on the id
+      this.initForm();
+    });
+  }
+
+  initForm() {
+    this.directoresForm = this.formBuilder.group({
+      nombre: [this.directores?.nombre || '', [Validators.required]],
+      nacionalidad: [this.directores?.nacionalidad || '', [Validators.required]],
+      fecha_nacimiento: [this.directores?.f_nacimiento || '', [Validators.required]],
+      biografia: [this.directores?.biografia || '', [Validators.required]],
+    });
+  }
+
+  actualizarDirector(directores: Directores): void {
+    this.DirectoresService.editarDirector(directores, this.directores.idDirector).subscribe((directores) => {
+      alert('Registro actualizado con éxito');
+    });
+  }
 }
