bd5347fc794c687a03013c8c63498a047bf7dfa2
Zare <zarellaburbanov@gmail.com>
2024-05-30 10:57:15 -0500

    eliminar director listo

--- src/app/directores/eliminar-director/eliminar-director.component.ts
+++ src/app/directores/eliminar-director/eliminar-director.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormArray,FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Directores } from '../directores';
+import { DirectoresService} from '../directores.service';
 
 @Component({
   selector: 'app-eliminar-director',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './eliminar-director.component.html',
-  styleUrl: './eliminar-director.component.css'
+  styleUrls: ['./eliminar-director.component.css']
 })
-export class EliminarDirectorComponent {
+export class EliminarDirectorComponent implements OnInit {
+  directorForm!: FormGroup;
+  id!: number;
+
+  constructor(private formBuilder: FormBuilder, private DirectoresService: DirectoresService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.directorForm = this.formBuilder.group({
+      id: [this.id, [Validators.required]]
+    })
+  }
+
+  eliminarDirector(Director: Directores): void {
+    this.DirectoresService.eliminarDirector(Director.idDirector).subscribe(
+      () => {
+        alert('director eliminado con éxito');
+        this.router.navigate(['/director']);
+      }
+    );
+  }
 
 }
