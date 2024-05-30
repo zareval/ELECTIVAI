119ce7d810ef56a498fe7c0af13312aad4bc59bc
Zare <zarellaburbanov@gmail.com>
2024-05-30 15:30:44 -0500

    Peliculas editar listo

--- src/app/peliculas/peliculas-editar/peliculas-editar.component.spec.ts
+++ src/app/peliculas/peliculas-editar/peliculas-editar.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeliculasEditarComponent } from './peliculas-editar.component';
+import {RouterTestingModule} from '@angular/router/testing'
+import { AppComponent } from '../../app.component';
+
+
+describe('AppComponent',() =>{
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations:[AppComponent]
+  }));
+
+  it('should create the app', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app).toBeTruthy();
+  })
+})
+
 
 describe('PeliculasEditarComponent', () => {
   let component: PeliculasEditarComponent;
--- src/app/peliculas/peliculas-editar/peliculas-editar.component.ts
+++ src/app/peliculas/peliculas-editar/peliculas-editar.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Peliculas } from '../peliculas';
+import { PeliculasService } from '../peliculas.service';
 
 @Component({
-  selector: 'app-peliculas-editar',
+  selector: 'app-editar-peliculas',
   standalone: true,
-  imports: [],
-  templateUrl: './peliculas-editar.component.html',
-  styleUrl: './peliculas-editar.component.css'
+  imports: [ReactiveFormsModule], // Assuming you're using Reactive Forms
+  templateUrl: './editar-peliculas.component.html',
+  styleUrls: ['./editar-peliculas.component.css']
 })
-export class PeliculasEditarComponent {
+export class PeliculasEditarComponent implements OnInit {
+  PeliculasForm!: FormGroup;
+  Peliculas!: Peliculas;
+  idPeliculas: string = '';
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private PeliculasService: PeliculasService
+  ) {
+    this.idPeliculas = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.PeliculasService.listarPeliculas().subscribe(pe => {
+      this.Peliculas = pe.find(Peliculas => Peliculas.idPelicula === parseInt(this.idPeliculas))!; // Get the specific Peliculas based on the id
+      this.initForm();
+    });
+  }
+
+  initForm() {
+    this.PeliculasForm = this.formBuilder.group({
+      nombre: [this.Peliculas?.nombre || '', [Validators.required]],
+      genero: [this.Peliculas?.genero || '', [Validators.required]],
+      duracion: [this.Peliculas?.duracion || '', [Validators.required]],
+      pais: [this.Peliculas?.pais || '', [Validators.required]],
+      f_estreno: [this.Peliculas?.f_estreno || '', [Validators.required]],
+      trailer:[this.Peliculas?.trailer || '', [Validators.required]],
+      poster: [this.Peliculas?.poster || '', [Validators.required]],
+      director: [this.Peliculas?.director || '', [Validators.required]],
+      protagonistas: [this.Peliculas?.protagonistas || '', [Validators.required]],
+    });
+  }
+
+  actualizarPeliculas(peliculas: Peliculas): void {
+    this.PeliculasService.editarPeliculas(peliculas, this.Peliculas.idPelicula).subscribe((Peliculas) => {
+      alert('Registro actualizado con éxito');
+    });
+  }
 }
